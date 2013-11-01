fd067e205d54c269aa6f3138cec22751b106991c
unknown <iguest@IS-M334L23-03.ischool.uw.edu>
2013-11-01 11:54:16 -0700

    base lab

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,39 @@
 //signup.js 
 // add your JavaScript code to this file
 
+$(function() {
+	var stateSelect = $('.us-states');
+	var idx;
+	var state;
+	for (idx = 0; idx < usStates.length; ++idx) {
+	    state = usStates[idx];
+	    //code to create new option element, populate, and append
+	    option = $(document.createElement('option'));
+	    option.attr('value', state.abbreviation);
+	    option.html(state.name);
+	    stateSelect.append(option);
+	}
+});
+
+$('.signup-form').submit(function(){
+    //code to execute when the sign-up form is submitted
+    //this is the raw DOM form element
+    //wrap it in a jQuery object so we can use jQuery methods on it
+    var signupForm = $(this);
+	//select a descendant input element with the name "addr-1"
+    var addr1Input = signupForm.find('input[name="addr-1"]');
+    var addr1Value = addr1Input.val();
+    if(addr1Value.length > 0) {
+    	var zippy = signupForm.find('input[name="zip"]');
+    	var zipzip = zippy.val();
+    	if(zipzip.length == 0) {
+    		alert("DEMON FORM DEMANDS YOUR ZIP CODE, MORTAL");
+    		return false;
+    	}
+    }
+});
+
+$('.cancel-signup').click(function(){
+    //code to run when user clicks "No Thanks!" button
+    window.location = 'http://www.google.com';
+}); //cancel-signup click
